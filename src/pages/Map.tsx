ea83827141cf4e084e0bb9f637732be7eebaaa9f
Ahmed Amdagas <40519703+barack300@users.noreply.github.com>
2025-11-03 22:23:58 +0100

    Fix JSX syntax in Map.tsx

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -284,7 +284,7 @@ const Map = () => {
               loading={locationLoading}
               error={locationError}
             /> 
-            }}
+            )}
           </CardContent>
         </Card>
 
