16ab92713cd32513c8d587819e56e74142d843c2
Ahmed Amdagas <40519703+barack300@users.noreply.github.com>
2025-11-17 12:07:36 +0100

    Update index.ts

--- supabase/functions/verify-payment/index.ts
+++ supabase/functions/verify-payment/index.ts
@@ -1,6 +1,6 @@
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { createClient } from "npm:@supabase/supabase-js@2.36.0";
+import { createClient } from "npm:@supabase/supabase-js@2";
 // WARNING: This is a DANGEROUS client.
 // It has service_role privileges and can bypass RLS.
 // NEVER expose this to the client.
